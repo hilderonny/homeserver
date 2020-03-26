f995cef32b17c0e02b5a6f979c3c760c4427e277
hilderonny <hilderonny2014@gmail.com>
2020-03-26 06:55:47 +0000

    Aktualisieren install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+rm -f /etc/motd
 ln -s /gitlab/hilderonny/homeserver/loginbanner /etc/motd
 echo "Banner /gitlab/hilderonny/homeserver/loginbanner" >> /etc/ssh/sshd_config
-rm /etc/issue
+rm -f /etc/issue
 ln -s /gitlab/hilderonny/homeserver/loginbanner /etc/issue
